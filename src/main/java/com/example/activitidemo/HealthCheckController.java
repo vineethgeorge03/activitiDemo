f5c7bb6e19663190d40ab1c035c58499b25bca40
vineeth <vineeth@mitter.io>
2019-12-02 08:57:32 +0530

    initial commit

---
+++ src/main/java/com/example/activitidemo/HealthCheckController.java
@@ -0,0 +1,18 @@
+package com.example.activitidemo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HealthCheckController {
+
+    @GetMapping("/healthCheck")
+    public ResponseEntity healthCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
